fcb3f0d4af4ffa6268a359877a53e0558e16a9c1
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 11:49:52 -0300

    Criando a estrutura das tabelas do banco de dados no arquivo AppDbContext.

--- scr/BibliotecaData/Data/AppDbContext.cs
+++ scr/BibliotecaData/Data/AppDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace BibliotecaData.Data
+﻿using BibliotecaBusiness.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+namespace BibliotecaData.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        public AppDbContext(DbContextOptions options): base(options) {  }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
