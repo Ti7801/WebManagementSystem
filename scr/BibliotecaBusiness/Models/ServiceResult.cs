82294cdbc0d0e40703857624b7fc13e5fd2539cf
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 16:37:20 -0300

    Criação da classe serviceResult.cs

---
+++ scr/BibliotecaBusiness/Models/ServiceResult.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliotecaBusiness.Models
+{
+    public class ServiceResult
+    {
+        public bool Success { get; set; }
+        public List<string> Erros { get; set; }
+
+        public ServiceResult()
+        {
+            this.Erros = new List<string>();
+        }
+    }
+}
