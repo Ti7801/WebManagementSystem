793fbd339713c68957251faabaef187178c31551
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 14:43:54 -0300

    Implementando as TableConfigs de Usuario e de Tarefa para formulação do banco de dados.

---
+++ scr/BibliotecaData/TableConfig/TarefaTableConfig.cs
@@ -0,0 +1,32 @@
+﻿using BibliotecaBusiness.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibliotecaData.TableConfig
+{
+    public class TarefaTableConfig : IEntityTypeConfiguration<Tarefa>
+    {
+        public void Configure(EntityTypeBuilder<Tarefa> builder)
+        {
+            builder.ToTable("Tarefa");
+
+            builder.HasKey(tarefa => tarefa.Id);
+
+            builder.Property(tarefa => tarefa.Descricao)
+                .IsRequired()
+                .HasMaxLength(300);
+
+            builder.Property(tarefa => tarefa.DataCriacao)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(tarefa => tarefa.DataConclusao)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(tarefa => tarefa.StatusTarefa)
+                .IsRequired()
+                .HasMaxLength(15);
+        }
+    }
+}
---
+++ scr/BibliotecaData/TableConfig/UsuarioTableConfig.cs
@@ -0,0 +1,45 @@
+﻿using BibliotecaBusiness.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibliotecaData.TableConfig
+{
+    public class UsuarioTableConfig : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(usuario => usuario.Id);
+
+            builder.Property(usuario => usuario.Nome)
+                .IsRequired()
+                .HasMaxLength(300);
+
+            builder.Property(usuario => usuario.Nascimento)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(usuario => usuario.Telefone)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(usuario => usuario.Email)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            builder.Property(usuario => usuario.Endereco)
+              .IsRequired()
+              .HasMaxLength(15);
+
+            builder.Property(usuario => usuario.DataCriacao)
+              .IsRequired()
+              .HasMaxLength(15);
+
+            builder.HasMany<Tarefa>()
+                .WithOne()
+                .HasForeignKey(tarefa => tarefa.UsuarioId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
