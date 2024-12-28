51a15bb0639bfc9c643324300f67af4ed5817487
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 12:42:43 -0300

    Implementando a injeção de dependencias no arquivo Program.cs

--- scr/WebManagementSystem/Program.cs
+++ scr/WebManagementSystem/Program.cs
@@ -1,7 +1,29 @@
+using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Services;
+using BibliotecaData.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(
+    (DbContextOptionsBuilder optionsBuilder) =>
+    {
+        string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+    },
+    ServiceLifetime.Scoped
+);
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<CadastrarUsuarioService>();
+builder.Services.AddScoped<ConsultarUsuarioService>();
+
+builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+builder.Services.AddScoped<CadastrarTarefaService>();
+builder.Services.AddScoped<ConsultarTarefaService>();
 
 var app = builder.Build();
 
