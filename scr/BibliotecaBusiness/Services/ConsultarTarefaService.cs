ae13a14b677b8c8971a0640a3d1f07f21a9c7a40
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 16:51:38 -0300

    Criação do serviço ConsultarTarefaService.

---
+++ scr/BibliotecaBusiness/Services/ConsultarTarefaService.cs
@@ -0,0 +1,32 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
+{
+    public class ConsultarTarefaService
+    {
+        private readonly ITarefaRepository tarefaRepository;
+
+        public ConsultarTarefaService(ITarefaRepository tarefaRepository)
+        {
+            this.tarefaRepository = tarefaRepository;
+        }
+
+        public ServiceResult ConsultarTarefa(Guid id)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try
+            {
+                tarefaRepository.ObterTarefa(id);
+                serviceResult.Success = true;
+            }
+            catch (Exception e)
+            {
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+            }
+            return serviceResult;
+        }
+    }
+}
