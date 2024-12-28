cc01cd46ff57a10b95322f9dfd1325934f943345
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 16:50:57 -0300

    Criação do serviço CadastrarUsuarioService.

---
+++ scr/BibliotecaBusiness/Services/CadastrarUsuarioService.cs
@@ -0,0 +1,33 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
+{
+    public class CadastrarUsuarioService
+    {
+        private readonly IUsuarioRepository usuarioRepository;
+
+        public CadastrarUsuarioService(IUsuarioRepository usuarioRepository)
+        {
+            this.usuarioRepository = usuarioRepository; 
+        }
+
+        public ServiceResult CadastrarUsuario(Usuario usuario)
+        {
+            ServiceResult serviceResult = new ServiceResult();  
+
+            try
+            {
+                usuarioRepository.AdicionarUsuario(usuario);
+                serviceResult.Success = true;
+            }
+            catch (Exception e)
+            {
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+
+            }
+            return serviceResult;
+        }
+    }
+}
