3a7fc95822b83d99490d49b1913d8111fbb17218
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 16:53:35 -0300

    Criação do serviço ConsultarUsuarioService.

---
+++ scr/BibliotecaBusiness/Services/ConsultarUsuarioService.cs
@@ -0,0 +1,32 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
+{
+    public class ConsultarUsuarioService
+    {
+        private readonly IUsuarioRepository usuarioRepository;
+
+        public ConsultarUsuarioService(IUsuarioRepository usuarioRepository)
+        {
+            this.usuarioRepository = usuarioRepository;
+        }
+
+        public ServiceResult ConsultarUsuario(Guid id)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try
+            {
+                usuarioRepository.ObterUsuario(id);
+                serviceResult.Success = true;
+            }
+            catch (Exception e)
+            {
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+            }
+            return serviceResult;
+        }
+    }
+}
