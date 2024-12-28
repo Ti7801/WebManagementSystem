5c8b0f63c5736034ca91de2e361b25473f2705ae
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 11:44:07 -0300

    Fazendo pequenas correções na model Usuario.

--- scr/BibliotecaBusiness/Models/Usuario.cs
+++ scr/BibliotecaBusiness/Models/Usuario.cs
@@ -4,21 +4,12 @@ namespace BibliotecaBusiness.Models
 {
     public class Usuario
     {
-        public long Id { get; set; }
-        [Required]
+        public Guid Id { get; set; }
         public string Nome { get; set; }
-        [Required]
         public DateTime Nascimento { get; set; }
-        [Required]
         public string Telefone { get; set; }
-        [Required]
         public string Email { get; set; }
-        [Required]
         public string Endereco { get; set; }
-        [Required]
-        public string Senha { get; set; }
-        [Required]
-        public TipoUsuario TipoUsuario { get; set; }
         public DateTime DataCriacao { get; set; }
     }
 }
