d3055adec43346029f53b65d4077724f45297139
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 16:49:29 -0300

    Criação do serviço CadastrarTarefaService

---
+++ scr/BibliotecaBusiness/Services/CadastrarTarefaService.cs
@@ -0,0 +1,32 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
+{
+    public class CadastrarTarefaService
+    {
+        private readonly ITarefaRepository tarefaRepository;
+
+        public CadastrarTarefaService(ITarefaRepository tarefaRepository)
+        {
+            this.tarefaRepository = tarefaRepository;
+        }
+
+        public ServiceResult CadastrarTarefa(Tarefa tarefa)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try
+            {
+                tarefaRepository.AdicionarTarefa(tarefa);
+                serviceResult.Success = true;
+            }
+            catch (Exception e)
+            {
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+            }
+            return serviceResult;
+        }
+    }
+}
