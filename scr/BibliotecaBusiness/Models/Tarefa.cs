0f319a58a73454c8921296ab7a1630ef7cff78d1
Ti7801 <tiagodaltro19@gmail.com>
2024-12-28 11:43:31 -0300

    Fazendo pequenas correções na Model Tarefa.

--- scr/BibliotecaBusiness/Models/Tarefa.cs
+++ scr/BibliotecaBusiness/Models/Tarefa.cs
@@ -4,13 +4,11 @@ namespace BibliotecaBusiness.Models
 {
     public class Tarefa
     {
-        public long Id { get; set; }
-        [Required]
-        public string Message { get; set; }
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataConclusao { get; set; }
-        [Required]
         public StatusTarefa StatusTarefa { get; set; }
-        public long GestorId { get; set; }
+        public Guid GestorId { get; set; }
     }
 }
