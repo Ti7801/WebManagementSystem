ed6699306d7d083abe63b79ced79e572f2464fd7
Ti7801 <tiagodaltro19@gmail.com>
2024-12-29 11:18:55 -0300

    Criação do diretório ViewModels e da classe UsuarioViewModel.cs

---
+++ scr/WebManagementSystem/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace WebManagementSystem.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime Nascimento { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Email { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Foto { get; set; }
+
+        public string Senha { get; set; }
+
+        public bool EhGestor { get; set; }
+    }
+}
