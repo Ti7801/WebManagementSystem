538bfe70649cc0ab3c774cb104cd4d7b48eec0f9
Ti7801 <tiagodaltro19@gmail.com>
2024-12-29 10:50:46 -0300

    Fazendo pequena alteração em uma das propriedades da Model tarefa.

--- scr/BibliotecaBusiness/Models/Tarefa.cs
+++ scr/BibliotecaBusiness/Models/Tarefa.cs
@@ -5,7 +5,7 @@ namespace BibliotecaBusiness.Models
     public class Tarefa
     {
         public Guid Id { get; set; }
-        public string Descricao { get; set; }
+        public string Messagem { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataConclusao { get; set; }
         public StatusTarefa StatusTarefa { get; set; }
