591feef55d6d61176506d71126ce89b381f6c37e
Ti7801 <tiagodaltro19@gmail.com>
2024-12-29 20:37:40 -0300

    Tornando algumas propriedades de TarefaViewModel obrigatórias.

---
+++ scr/WebManagementSystem/ViewModels/TarefaViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BibliotecaBusiness.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebManagementSystem.ViewModels
+{
+    public class TarefaViewModel
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Messagem { get; set; }
+        [Required]
+        public DateTime DataCriacao { get; set; }
+        [Required]
+        public  DateTime DataConclusao { get; set; }
+        [Required]
+        public StatusTarefa StatusTarefa { get; set; }
+        [Required]
+        public Guid GestorId { get; set; }
+
+        public Guid? UsuarioId { get; set; }
+    }
+}
