677a325857f786701c680ebd862d395f6e360b63
Ti7801 <tiagodaltro19@gmail.com>
2024-12-29 11:04:36 -0300

    Criação de um novo arquivo UsuarioService contendo o serviço de ConsultarTarefa e CadastrarTarefa.

---
+++ scr/BibliotecaBusiness/Services/TarefaService.cs
@@ -0,0 +1,57 @@
+﻿using BibliotecaBusiness.Models;
+using Microsoft.Extensions.Logging;
+using BibliotecaBusiness.Abstractions;
+
+namespace BibliotecaBusiness.Services
+{
+    public class TarefaService
+    {
+        private readonly ITarefaRepository tarefaRepository;
+        private readonly ILogger<TarefaService> logger;  
+
+        public TarefaService(ITarefaRepository tarefaRepository,
+                            ILogger<TarefaService> logger)
+        {
+            this.tarefaRepository = tarefaRepository;
+            this.logger = logger;   
+        }
+
+        public ServiceResult CadastrarTarefa(Tarefa tarefa)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try
+            {
+                tarefaRepository.AdicionarTarefa(tarefa);
+                serviceResult.Success = true;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.ToString());
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+            }
+            return serviceResult;
+        }
+
+        public ServiceResult<Tarefa> ConsultarTarefa(Guid id)
+        {
+            ServiceResult<Tarefa> serviceResult = new ServiceResult<Tarefa>();
+
+            try
+            {
+                Tarefa? tarefa = tarefaRepository.ObterTarefa(id);
+                serviceResult.Value = tarefa;
+                serviceResult.Success = true;
+                return serviceResult;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.ToString());
+                serviceResult.Success = false;
+                return serviceResult;
+            }
+        }
+    }
+}
+
