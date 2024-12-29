4804f25f378889b07f496858f61ce78e18352e35
Ti7801 <tiagodaltro19@gmail.com>
2024-12-29 11:05:56 -0300

    Criação de um novo arquivo UsuarioService contendo o serviço de ConsultarUsuario e CadastrarUsuario.

---
+++ scr/BibliotecaBusiness/Services/UsuarioService.cs
@@ -0,0 +1,56 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+using Microsoft.Extensions.Logging;
+
+namespace BibliotecaBusiness.Services
+{
+    public class UsuarioService
+    {
+        private readonly IUsuarioRepository usuarioRepository;
+        private readonly ILogger<UsuarioService> logger;
+
+        public UsuarioService(IUsuarioRepository usuarioRepository,
+                              ILogger<UsuarioService> logger)
+        {
+            this.usuarioRepository = usuarioRepository; 
+            this.logger = logger;
+        }
+
+        public ServiceResult CadastrarUsuario(Usuario usuario)
+        {
+            ServiceResult serviceResult = new ServiceResult();  
+
+            try
+            {
+                usuarioRepository.AdicionarUsuario(usuario);
+                serviceResult.Success = true;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.ToString());
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+            }
+            return serviceResult;
+        }
+
+        public ServiceResult<Usuario> ConsultarUsuario(Guid id)
+        {
+            ServiceResult<Usuario> serviceResult = new ServiceResult<Usuario>();
+
+            try
+            {
+                Usuario? usuario = usuarioRepository.ObterUsuario(id);
+                serviceResult.Value = usuario;
+                serviceResult.Success = true;
+                return serviceResult;  
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.ToString());
+                serviceResult.Success = false;
+                return serviceResult; 
+            }
+        }
+    }
+}
