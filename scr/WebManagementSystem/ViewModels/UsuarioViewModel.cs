e2e1bb6b300f1f91619b4414bfbfb7c585eda304
Ti7801 <tiagodaltro19@gmail.com>
2024-12-29 20:33:14 -0300

    Tornando alguns campos de UsuarioViewModel obrigatórios.

--- scr/WebManagementSystem/ViewModels/UsuarioViewModel.cs
+++ scr/WebManagementSystem/ViewModels/UsuarioViewModel.cs
@@ -1,25 +1,27 @@
-﻿namespace WebManagementSystem.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebManagementSystem.ViewModels
 {
     public class UsuarioViewModel
     {
         public Guid Id { get; set; }
-
+        [Required]
         public string Nome { get; set; }
-
+        [Required]
         public DateTime Nascimento { get; set; }
-
+        [Required]
         public string Telefone { get; set; }
-
+        [Required]
         public string Celular { get; set; }
-
+        [Required]
         public string Email { get; set; }
-
+        [Required]
         public string Endereco { get; set; }
-
+        [Required]
         public string Foto { get; set; }
-
+        [Required]
         public string Senha { get; set; }
-
+        [Required]
         public bool EhGestor { get; set; }
     }
 }
