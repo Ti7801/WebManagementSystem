b3525ae9ee2847efeb692a2c293a904adee942c2
Ti7801 <tiagodaltro19@gmail.com>
2024-12-29 10:54:49 -0300

    Implementando um tipo genérico a ServiceResult para uso nos serviços.

--- scr/BibliotecaBusiness/Models/ServiceResult.cs
+++ scr/BibliotecaBusiness/Models/ServiceResult.cs
@@ -10,4 +10,18 @@
             this.Erros = new List<string>();
         }
     }
+
+    public class ServiceResult<T>
+    {
+        public bool Success { get; set; }
+
+        public List<string> Erros { get; set; }
+
+        public T? Value { get; set; }
+
+        public ServiceResult()
+        {
+            this.Erros = new List<string>();
+        }
+    }
 }
