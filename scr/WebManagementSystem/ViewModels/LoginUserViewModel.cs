157688f2e99d72a772713c43ab64561c797ae535
Ti7801 <tiagodaltro19@gmail.com>
2024-12-31 01:51:22 -0300

    Criando uma ViewModel para login.

---
+++ scr/WebManagementSystem/ViewModels/LoginUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebManagementSystem.ViewModels
+{
+    public class LoginUserViewModel
+    {
+        [Display(Name = "email")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está formato inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "senha")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha {  get; set; }
+
+
+    }
+}
