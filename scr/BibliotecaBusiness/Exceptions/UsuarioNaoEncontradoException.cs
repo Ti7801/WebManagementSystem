6cea77dcdf466f03332c34e71aabd10563fb0d41
Ti7801 <tiagodaltro19@gmail.com>
2025-01-02 17:18:51 -0300

    Adicionando uma nova Exception UsuarioNaoEncontradoException no diretório de Exceptions.

--- scr/BibliotecaBusiness/Exceptions/UsuarioNaoEncontradoException.cs
+++ scr/BibliotecaBusiness/Exceptions/UsuarioNaoEncontradoException.cs
@@ -1,6 +1,9 @@
 ﻿namespace BibliotecaBusiness.Exceptions
 {
-    public class UsuarioNaoEncontradoException
+    public class UsuarioNaoEncontradoException : Exception
     {
+        public UsuarioNaoEncontradoException() { }
+        public UsuarioNaoEncontradoException(string message) : base(message) { }
+        public UsuarioNaoEncontradoException(string message, Exception e) : base(message, e) { }
     }
 }
