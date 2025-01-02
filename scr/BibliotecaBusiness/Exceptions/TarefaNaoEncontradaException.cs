d3a47b25e6c734f228fb51d5991407e406968521
Ti7801 <tiagodaltro19@gmail.com>
2025-01-02 17:17:32 -0300

    Adicionando uma nova Exception TarefaNaoEncontradaException no diretorio de Exceptions.

--- scr/BibliotecaBusiness/Exceptions/TarefaNaoEncontradaException.cs
+++ scr/BibliotecaBusiness/Exceptions/TarefaNaoEncontradaException.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace BibliotecaBusiness.Exceptions
 {
-    public class TarefaNaoEncontradaException
+    public class TarefaNaoEncontradaException : Exception
     {
+        public TarefaNaoEncontradaException() { }
+        public TarefaNaoEncontradaException(string message) : base(message) { }
+        public TarefaNaoEncontradaException(string message, Exception e) : base(message, e) { }
     }
 }
