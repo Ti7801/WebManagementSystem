8a28be5bfbc444b07f95527689e82dbd3ee44c3b
Ti7801 <tiagodaltro19@gmail.com>
2025-01-03 01:55:33 -0300

    Adicionando nova role de subordinado a Action e corrigindo redirecionamento.

--- scr/WebManagementSystem/Controllers/HomeController.cs
+++ scr/WebManagementSystem/Controllers/HomeController.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebManagementSystem.Controllers
 {
-    [Authorize(Roles = "GestorAdmin, Gestor")]
+    [Authorize(Roles = "GestorAdmin, Gestor, Subordinado")]
     public class HomeController : Controller
-    {
-        
+    {       
         [HttpGet]
         public ActionResult Index()
         {
-            return View();  
+            
+            return RedirectToAction("ConsultarTarefas", "Tarefa");  
         }
     }
 }
