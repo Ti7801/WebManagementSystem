db93f8707369f7f538fabf4557d2546f0afc51ab
Ti7801 <tiagodaltro19@gmail.com>
2025-01-04 11:35:37 -0300

    Tornando alguns campos da model usuario obrigatórios.

--- scr/BibliotecaBusiness/Models/Usuario.cs
+++ scr/BibliotecaBusiness/Models/Usuario.cs
@@ -5,12 +5,19 @@ namespace BibliotecaBusiness.Models
     public class Usuario
     {
         public Guid Id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Required]
         public DateTime Nascimento { get; set; }
+        [Required]
         public string Telefone { get; set; }
+        [Required]
         public string Celular {  get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Endereco { get; set; }
-        public string Foto { get; set; } = string.Empty;
+        [Required]
+        public string Foto { get; set; } 
     }
 }
